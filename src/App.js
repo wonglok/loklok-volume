74d730108e7a95a12d8695841c56fd9f76619ea9
wonglok <yellowhappy831@gmail.com>
2021-04-01 09:32:37 +0800

    go

--- src/App.js
+++ src/App.js
@@ -135,7 +135,7 @@ function App() {
             <Link to={"/life-energy-water"}>Life Energy Water</Link>
           </div>
           <div className={"m-5"}>
-            <Link to={"/line-stuff"}>Line Stuff</Link>
+            <Link to={"/line-stuff"}>Line Stuff (broken)</Link>
           </div>
           <div className={"m-5"}>
             <Link to={"/line-drop"}>Line Drop</Link>
